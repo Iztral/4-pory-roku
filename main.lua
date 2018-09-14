43c583124aa2d90d1385a87cd5069f5493811069
Aleksander Krawiel <aleksander.krawiel@kastudios.pl>
2018-09-14 20:31:52 +0200

    Just another hello world

--- main.lua
+++ main.lua
@@ -1,3 +1,12 @@
+local testFont = love.graphics.newFont(24)
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.setFont(testFont)
+    love.graphics.printf("Hello World", 0, love.graphics.getHeight() / 2 - 12, love.graphics.getWidth(), "center")
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
 end
