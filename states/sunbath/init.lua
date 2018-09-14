dfd56ed4c7982a0841045b24a01cec1dfd302b6e
Iztral <32697140+Iztral@users.noreply.github.com>
2018-09-14 22:34:51 +0200

    wat2

--- states/sunbath/init.lua
+++ states/sunbath/init.lua
@@ -2,5 +2,5 @@ return {
     init = function(self)
         love.graphics.setBackgroundColor(0.25, 0.25, 0.25)
     end,
-    draw = require "states.pumpkins.draw"
+    draw = require "states.sunbath.draw"
 }
