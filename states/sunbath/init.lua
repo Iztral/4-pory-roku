90a269e43b6b2499d360c74aeffba374be84c3d1
Iztral <32697140+Iztral@users.noreply.github.com>
2018-09-15 00:04:32 +0200

    sun in progress

--- states/sunbath/init.lua
+++ states/sunbath/init.lua
@@ -1,6 +1,14 @@
 return {
     init = function(self)
         love.graphics.setBackgroundColor(0.25, 0.25, 0.25)
+		self.player = {
+		player1 = {player_id = 0, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
+		player2 = {player_id = 1, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
+		player3 = {player_id = 2, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
+		player4 = {player_id = 3, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
+		}
+		self.timer = 30
     end,
     draw = require "states.sunbath.draw"
+	update = require "states.sunbath.update"
 }
