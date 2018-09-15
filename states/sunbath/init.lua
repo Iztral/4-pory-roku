1ebe989859d5ad63e8dd0d5c419a72509884d194
Iztral <32697140+Iztral@users.noreply.github.com>
2018-09-15 12:29:36 +0200

    triggered

--- states/sunbath/init.lua
+++ states/sunbath/init.lua
@@ -1,11 +1,11 @@
 return {
     init = function(self)
         love.graphics.setBackgroundColor(0.25, 0.25, 0.25)
-		self.player = {
-		player1 = {player_id = 0, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
-		player2 = {player_id = 1, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
-		player3 = {player_id = 2, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
-		player4 = {player_id = 3, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = 0, rotation_old = 0},
+		self.players = {
+		player1 = {player_id = 0, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = nil, rotation_old = nil},
+		player2 = {player_id = 1, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = nil, rotation_old = nil},
+		player3 = {player_id = 2, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = nil, rotation_old = nil},
+		player4 = {player_id = 3, player_score = 0, turn_grade = 0, angle_difference = 0, rotation_new = nil, rotation_old = nil},
 		}
 		self.timer = 30
 		
