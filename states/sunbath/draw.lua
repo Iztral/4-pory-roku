d33b597d7d5a466c89f6d3838637de71ebd380a6
Iztral <32697140+Iztral@users.noreply.github.com>
2018-09-15 18:19:23 +0200

    anim fin

--- states/sunbath/draw.lua
+++ states/sunbath/draw.lua
@@ -1,10 +1,10 @@
 return function(self)
-    love.graphics.setColor(1, 1, 0)
-    
+    love.graphics.draw(self.sand,0,0)
 	love.graphics.print(string.format("%.0f", self.timer or 0),10, 10)
 	love.graphics.print(string.format("%.0f", self.players.player1.player_score or 0),30,30)
-	love.graphics.print(self.players.player1.angle_difference,120,120)
 	love.graphics.print(string.format("%.0f", self.players.player2.player_score or 0),50,50)
 	love.graphics.print(string.format("%.0f", self.players.player3.player_score or 0),70,70)
 	love.graphics.print(string.format("%.0f", self.players.player4.player_score or 0),90,90)
+	love.graphics.print(self.turn_direction, 120,120)
+	love.graphics.print(self.bck_timer, 150,150)
 end
