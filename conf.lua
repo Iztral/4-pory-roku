2fce0bbc16ea903570c16fe03b4037298b262d29
akrw <a.krawiel@gmail.com>
2018-10-08 04:03:25 +0200

    Preparing for release

--- conf.lua
+++ conf.lua
@@ -4,4 +4,18 @@ function love.conf(t)
     t.window.title = "4 pory"
     t.window.fullscreen = true
     t.window.fullscreenType = "desktop"
+    t.releases = {
+        title = "4pory",
+        package = "4pory",
+        loveVersion = "11.1",
+        version = "1.0.0",
+        author = "To-pory",
+        email = nil,
+        description = "A game",
+        homepage = nil,
+        identifier = nil,
+        compile = true,
+        excludeFileList = {"_OLD", "TODO.md", "README.md", ".git", ".gitattributes"},
+        releaseDirectory = "../4pory-release",
+    }
 end
